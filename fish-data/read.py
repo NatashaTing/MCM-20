450bbd858b761a9a387cbe3697f1dc701977b6ae
NatashaTing <natasha@zept.ca>
2020-02-15 21:03:00 -0700

    update

--- fish-data/read.py
+++ fish-data/read.py
@@ -1,4 +1,10 @@
 
+import datetime as dt  # Python standard library datetime  module
+import numpy as np
+from netCDF4 import Dataset  # http://code.google.com/p/netcdf4-python/
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap, addcyclic, shiftgrid
+
 import pandas as pd
 
 def main():
